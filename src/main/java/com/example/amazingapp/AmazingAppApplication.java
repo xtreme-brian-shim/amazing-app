4c72cba17e5ad7079bdc367d60b959d72a6e713e
Brian S <the.brian.shim@gmail.com>
2018-11-18 15:46:06 -0500

    initial commit

---
+++ src/main/java/com/example/amazingapp/AmazingAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.amazingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazingAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/amazingapp/AmazingAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.amazingapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmazingAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
